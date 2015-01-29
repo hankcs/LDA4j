2fe0aee6d49d7031b3f5dbe19ed57ddceb0b6d6d
hankcs <jfservice@126.com>
2015-01-29 20:52:53 +0800

    Initial commit

---
+++ src/test/java/com/hankcs/lda/TestCorpus.java
@@ -0,0 +1,51 @@
+/*
+ * <summary></summary>
+ * <author>He Han</author>
+ * <email>hankcs.cn@gmail.com</email>
+ * <create-date>2015/1/29 17:22</create-date>
+ *
+ * <copyright file="TestCorpus.java" company="上海林原信息科技有限公司">
+ * Copyright (c) 2003-2014, 上海林原信息科技有限公司. All Right Reserved, http://www.linrunsoft.com/
+ * This source is subject to the LinrunSpace License. Please contact 上海林原信息科技有限公司 to get more information.
+ * </copyright>
+ */
+package com.hankcs.lda;
+
+import junit.framework.TestCase;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author hankcs
+ */
+public class TestCorpus extends TestCase
+{
+    public void testAddDocument() throws Exception
+    {
+        List<String> doc1 = new ArrayList<String>();
+        doc1.add("hello");
+        doc1.add("word");
+        List<String> doc2 = new ArrayList<String>();
+        doc2.add("hankcs");
+        Corpus corpus = new Corpus();
+        corpus.addDocument(doc1);
+        corpus.addDocument(doc2);
+        System.out.println(corpus);
+    }
+
+    public void testLoad() throws Exception
+    {
+        // 1. Load corpus from disk
+        Corpus corpus = Corpus.load("data/mini");
+        // 2. Create a LDA sampler
+        LdaGibbsSampler ldaGibbsSampler = new LdaGibbsSampler(corpus.getDocument(), corpus.getVocabularySize());
+        // 3. Train it
+        ldaGibbsSampler.gibbs(10);
+        // 4. The phi matrix is a LDA model, you can use LdaUtil to explain it.
+        double[][] phi = ldaGibbsSampler.getPhi();
+        Map<String, Double>[] topicMap = LdaUtil.translate(phi, corpus.getVocabulary(), 10);
+        LdaUtil.explain(topicMap);
+    }
+}
